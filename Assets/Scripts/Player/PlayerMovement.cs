255b6eb8adb2e663dbaf912d06550a5cc0a82871
sepron777 <kares.filip@gmail.com>
2024-09-14 19:26:53 +0200

    movement fixes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,7 +12,8 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = 20.0f;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
-    public float trunSmoothVeleocity;
+    private float trunSmoothVeleocity;
+    public float smoothTime;
     public GameObject visor;
     public Transform Visual;
 
@@ -30,11 +31,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Awake()
     {
-
-            playerCamera = Camera.main;
-            playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y + cameraYOffset, transform.position.z);
-            playerCamera.transform.SetParent(transform);
-        
+        playerCamera = Camera.main;
     }
 
     void Start()
@@ -54,21 +51,13 @@ public class PlayerMovement : MonoBehaviour
         isRunning = Input.GetKey(KeyCode.LeftShift);
 
         // We are grounded, so recalculate move direction based on axis
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
+        Vector3 forward = playerCamera.transform.TransformDirection(Vector3.forward);
+        Vector3 right = playerCamera.transform.TransformDirection(Vector3.right);
 
         float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical")).normalized;
-
-        if (dir.magnitude>=0.1f)
-        {
-            float tar = Mathf.Atan2(dir.x, dir.z)*Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(Visual.transform.eulerAngles.y,tar,ref trunSmoothVeleocity,0.1f);
-            Visual.transform.rotation = Quaternion.Euler(0,angle,0);
-        }
 
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
@@ -84,18 +73,17 @@ public class PlayerMovement : MonoBehaviour
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        if (dir.magnitude >= 0.1f)
+        {
+            float tar = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(Visual.transform.eulerAngles.y, tar, ref trunSmoothVeleocity, smoothTime);
+            Visual.transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
-        /*
-        // Player and Camera rotation
-        if (canMove && playerCamera != null)
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            visor.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        }*/
     }
 
     public Camera GetCamera()
